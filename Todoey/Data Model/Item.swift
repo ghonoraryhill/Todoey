ccda219527f037e73f622376d3335aec8d58bce6
Peter Ehrenberg <peter.ehrenberg@paymentgroup.de>
2018-01-04 15:31:40 +0100

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,11 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Peter Ehrenberg on 04.01.18.
+//  Copyright © 2018 Peter Ehrenberg. All rights reserved.
+//
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
